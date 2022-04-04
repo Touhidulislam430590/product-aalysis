9bf6b7c10d5231e803faf6444a1e8c764ffb688a
Touhidulislam430590 <sanon.touhid980@gmail.com>
2022-04-04 16:03:43 +0600

    complete Dashboard page with linechart and barchart

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import './Dashboard.css';
+import { LineChart, Line, XAxis, YAxis, BarChart, Bar } from 'recharts';
 
 const Dashboard = () => {
+    const [data, setData] = useState([]);
+    useEffect( ()=>{
+        fetch('data.json')
+        .then(res => res.json())
+        .then(data => setData(data))
+    }, [])
     return (
         <div>
-            <h1>Hello from Dashboard</h1>
-        </div>
+            <h1>Business chart for KTM duke 1290 super adventure</h1>
+            <div className='dashboard-container'>    
+                <div className="chart-1">
+                    <h3>Sells per month</h3>
+                    <LineChart width={500} height={350} data={data}>
+                        <Line type="monotone" dataKey="sell" stroke="#8884d8" />
+                        <XAxis dataKey="month"></XAxis>
+                        <YAxis></YAxis>
+                    </LineChart>
+                </div>
+
+                <div className="chart-2">
+                    <h3>Investment Per month</h3>
+                    <BarChart width={500} height={350} data={data}>
+                        <Bar dataKey="investment" fill="#982322"></Bar>
+                        <XAxis dataKey="month"></XAxis>
+                        <YAxis></YAxis>
+                    </BarChart>
+                </div>
+            </div>
+       </div>
     );
 };
 
